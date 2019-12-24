7e891e6e46be14b072dcfaa4b9b217e7d03e63a4
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-12-24 17:42:13 -0600

    updated server

--- server.py
+++ server.py
@@ -1,41 +1,56 @@
 import cv2
-import sys 
 import socket
-import pickle
-import time
+from io import StringIO
+import numpy as np
+
 
 def main():
-    header = 10
     host = '192.168.1.228'
     port = 1050
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((host, port))
-    print('listening for connections')
     sock.listen()
-    print('got connection')
     conn, addr = sock.accept()
+    
+    try:
+        sendFrames(sock, conn, addr)
+    except:
+        raise
+        sock.close()
 
+def sendFrames(sock, conn, addr):
     cap = getSource()
-    while cap.isOpened():
+    while cap != False and cap.isOpened():
         ret, frame = cap.read()
-        cv2.imshow("frame", frame)
-
-        msg = pickle.dumps(frame)
-        msg = ('len{}:<{}'.format(msg,HEADERSIZE)).encode() + msg
-
-        print(msg)
-        conn.send(msg)
+        if ret == False:
+            break
+        send(conn, frame)
+        #cv2.imshow("frame", frame)
+        
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+    sock.close()
+
+def send(conn, image):
+    f = StringIO()
+    np.savez_compressed(f, frame=image)
+    f.seek(0)
+    out = f.read()
+    val = "{0}:".format(len(f.getvalue()))
+    out = val + out
+    conn.send(out)
 
 
 
 def getSource():
-    return cv2.VideoCapture(0)
+    try:
+        source = cv2.VideoCapture(0)
+        return source
+    except "VIDEOIO ERROR":
+        return False
 
 
 
 if __name__ == '__main__':
     main()
-
