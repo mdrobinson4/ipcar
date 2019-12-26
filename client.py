e73561b56432621ebd6a084ea17a3b2b5914634d
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-12-26 10:42:02 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,43 +1,38 @@
 import cv2
 import socket
-from io import StringIO
+from io import BytesIO
 import numpy as np
 
 def main():
     host = '192.168.1.228'
     port = 1050
     header = 10
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
-    print('connected')
     getFrames(sock)
-    
+
 def getFrames(sock):
     while True:
         receive(sock)
 
 def receive(sock):
-    length = None
-    ultimate_buffer = ""
     while True:
-        data = sock.recv(1024)
-        ultimate_buffer += data
-        if len(ultimate_buffer) == length:
-            break
+        length = None
+        buffer = b''
         while True:
-            if length is None:
-                if ':' not in ultimate_buffer:
-                    break
-                length_str, ignored, ultimate_buffer = ultimate_buffer.partition(':')
-                length = int(length_str)
-            if len(ultimate_buffer) < length:
+            data = sock.recv(1024)
+            if length == None:
+                dataLen, ignored, buffer = ultimate_buffer.partition(b':')
+                length = int(dataLen)
+            else:
+                buffer += data
+            print('total: {}, buffer: {}'.format(dataLen, len(buffer)))
+            if len(buffer) >= dataLen:
                 break
-            ultimate_buffer = ultimate_buffer[length:]
-            length = None
-            break
-        final_image = np.load(StringIO(ultimate_buffer))['frame']
+        print(buffer)
+        final_image = np.load(BytesIO(buffer))['frame']
         print('received image')
+        cv2.imshow("image")
         return
 
 
